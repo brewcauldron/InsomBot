c049c4e436e085819dcf9b4547cddc098ec7821b
garrettm <garrett@gingerlabs.com>
2015-08-27 00:55:21 -0700

    Change to /giphy and fix bugs!

--- insombot.js
+++ insombot.js
@@ -1,13 +1,15 @@
 var cc = require('config-multipaas'),
-    env = require('./env.json'),
-    Discord = require("discord.js"),
-    Giphy = require('giphy-wrapper')(env["giphy_key"]);
+  env = require('./env.json'),
+  Discord = require("discord.js"),
+  Giphy = require('giphy-wrapper')(env["giphy_key"]);
 
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
-var config_overrides = {PORT: server_port}
+var config_overrides = {
+  PORT: server_port
+}
 var config = cc(config_overrides);
 
 var mybot = new Discord.Client();
@@ -17,7 +19,7 @@ mybot.on("message", function (msg) {
   var message = msg.content;
 
   //keywords
-  var giphy = "g:";
+  var giphy = "/giphy ";
   var hatter = "hater";
 
   // Reply to direct mentions
@@ -27,8 +29,9 @@ mybot.on("message", function (msg) {
   }
 
   //Giphy
-  if (message.indexOf(giphy) > -1) {
-    var term = message.substring(message.indexOf(":") + 1).trim().replace(/\s/g, "+");
+  var giphyIndex = message.indexOf(giphy);
+  if (giphyIndex > -1) {
+    var term = message.substring(giphyIndex + giphy.length).trim().replace(/\s/g, "+");
 
     Giphy.search(term, 10, 0, function (err, data) {
       if (err) {
@@ -37,17 +40,22 @@ mybot.on("message", function (msg) {
 
       var items = data.data;
 
-      var item = items[Math.floor(Math.random() * items.length)];
-
-      mybot.reply(msg, item.url);
+      if (items.length > 0) {
+        var item = items[Math.floor(Math.random() * items.length)];
+        mybot.sendMessage(msg, item.url);
+      }
+      else {
+        var apology = "Sorry, I couldn't find any giphys for the term: " + term;
+        mybot.reply(msg, apology);
+      }
     });
 
     return;
   }
 
   //Hatter
-  if (message === hater) {
-    mybot.reply(msg, "https://pbs.twimg.com/media/CM5gg9YVAAAVMcn.png");
+  if (message === hatter) {
+    mybot.sendMessage(msg, "https://pbs.twimg.com/media/CM5gg9YVAAAVMcn.png");
 
     return;
   }
